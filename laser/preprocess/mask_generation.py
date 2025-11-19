1e347aa606c2138a7d5d559eb18b7ffb75e652c8
Kevin Xue <153964133+kevinxuez@users.noreply.github.com>
2025-11-19 15:32:08 -0500

    Disable bfloat16 autocast in mask_generation.py
    Comment out the autocast for bfloat16 in mask_generation.py

--- laser/preprocess/mask_generation.py
+++ laser/preprocess/mask_generation.py
@@ -13,7 +13,7 @@ from datetime import datetime
 from typing import Dict, List
 
 # use bfloat16 for the entire notebook
-torch.autocast(device_type="cuda", dtype=torch.bfloat16).__enter__()
+# torch.autocast(device_type="cuda", dtype=torch.bfloat16).__enter__()
 
 if torch.cuda.get_device_properties(0).major >= 8:
     # turn on tfloat32 for Ampere GPUs (https://pytorch.org/docs/stable/notes/cuda.html#tensorfloat-32-tf32-on-ampere-devices)
